fe0c82d03b62ddd32d0f96fdf80a1e4bb6bc25ea
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-04-17 03:08:50 +0100

    Create projects schema for projects page

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416233756) do
+ActiveRecord::Schema.define(version: 20180417020655) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.text "blurb"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "link"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
