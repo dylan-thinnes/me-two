c3374070b90bbdf8dfe0b392ff0cf9ce8eb3bbd6
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-04-17 20:46:14 +0100

    Minify javascript and css, Compress assets

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module MeTwo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.css_compressor = :yui
+    config.assets.js_compressor = :uglifier
+    config.assets.gzip = true
   end
 end
