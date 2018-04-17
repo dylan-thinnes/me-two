d40ea6aba3c88d22d40c0632521b5a828fd3ad0b
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-04-17 02:21:52 +0100

    Implement JSON/nolayout params and 404 handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
   end
 
   def article
-    @article = Article.find(params[:id])
+    @article = Article.find_by(id: params[:id])
+    json = request.query_parameters['json'] == "true"
+    if json
+      render json: @article.to_json
+      return
+    end
+    nolayout = request.query_parameters['nolayout'] == "true"
+    if nolayout
+      render "article", layout: false
+      return
+    end
   end
   protect_from_forgery with: :exception
 end
