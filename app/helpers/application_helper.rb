7fdb81535a46b44d74ebf796fc99681f8a0b7d4e
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-04-17 13:33:55 +0100

    Add markdown parsing for articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+require 'redcarpet'
 module ApplicationHelper
+  def to_markdown(text)
+    @renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true) if @renderer.nil?
+    @markdown = Redcarpet::Markdown.new(@renderer) if @markdown.nil?
+    @markdown.render(text).html_safe
+  end
 end
