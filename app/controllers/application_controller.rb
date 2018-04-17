18298d79034eb85bc63755bf29cfa2be96eb39cb
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-04-17 01:12:24 +0100

    Add Article page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   def index
   end
+
+  def article
+    @article = Article.find(params[:id])
+  end
   protect_from_forgery with: :exception
 end
