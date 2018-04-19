7de858a990693ce75db912150c92079a7d617aa6
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2018-04-19 12:02:41 +0100

    Bind event listeners to local links for SPA-style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,29 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require_tree .
+window.addEventListener("load", function () {
+    linkEls = document.getElementsByTagName("a");
+    links = [];
+    for (var ii = 0; ii < linkEls.length; ii++) links[ii] = linkEls[ii];
+    links.filter(referencesLocal).map(bindHandler);
+});
+
+/* Link event binding code. */
+// Check if an <a> node references a local path
+referencesLocal = function (a) {
+    // Use getAttribute instead of .href since .href coerces to full url
+    url = a.getAttribute("href");
+    
+    // If the link is a dummy, ignore it
+    if (url == undefined) return false;
+
+    // If the link has a URI scheme, consider it non-local
+    hasScheme = (/^\w+:\/\//).test(url)
+    return !hasScheme;
+}
+
+prevDefaultCallback = function (e) { e.preventDefault(); }
+// Binds a URI handler and calls preventDefault
+bindHandler = function (a) {
+    a.addEventListener("click", prevDefaultCallback);
+}
+
+/* URL getting and replacement */
